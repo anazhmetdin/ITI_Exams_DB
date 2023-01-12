6ee80cf970623bf5b815efefde164729c6929679
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-01-13 01:28:52 +0200

    Create selectAllTablesData.sql

---
+++ selectAllTablesData.sql
@@ -0,0 +1,19 @@
+declare s_cur cursor
+	for SELECT TABLE_NAME
+		FROM INFORMATION_SCHEMA.TABLES
+		WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='ITI_Exams' and TABLE_NAME != 'sysdiagrams'
+
+declare @name nvarchar(50)
+open s_cur 
+fetch s_cur into @name
+begin
+	While @@fetch_status=0
+	begin
+		declare @tmp nvarchar(200) = 'select * from ' + @name 
+
+		EXEC sp_executesql @tmp
+		fetch s_cur into @name
+	end
+end
+close s_cur
+deallocate s_cur
