3f2da37291d91bfb909c76d36da5c36f158addb2
anazhmetdin <anazhmetdin@gmail.com>
2023-01-12 17:44:54 +0200

    use output variable with insert exam

--- SPs/CRUD_Exam.sql
+++ SPs/CRUD_Exam.sql
@@ -19,27 +19,29 @@ AS
 	END CATCH
 GO
 -- SELECT TEST
+/*
 SelectExam 1, NULL, NULL;
 GO
 SelectExam NULL, NULL, '2022-6-30';
 GO
 SelectExam NULL, '2022-6-30', NULL;
 GO
+*/
 ---------------------------------------
 
 -- INSERT
-CREATE OR ALTER PROC InsertExam @date DATE
+CREATE OR ALTER PROC InsertExam @date DATE, @id INT OUTPUT
 WITH ENCRYPTION
 AS
 BEGIN
 	BEGIN TRY
-	IF (@date IS NULL OR @date < GETDATE())
-		SET @date = GETDATE()
+		IF (@date IS NULL OR @date < GETDATE())
+			SET @date = GETDATE()
 
-	INSERT INTO ITI_Exams.dbo.Exam (date) VALUES (@date)
+		INSERT INTO ITI_Exams.dbo.Exam (date) VALUES (@date)
 
-	SELECT SCOPE_IDENTITY()
-	
+		SET @id = SCOPE_IDENTITY()
+
 	END TRY  
 	BEGIN CATCH  
 		THROW 500, 'Could not insert exam', 16
@@ -47,10 +49,12 @@ BEGIN
 END;
 GO
 -- INSERT TEST
+/*
 InsertExam '2022-6-30';
 GO
 InsertExam NULL;
 GO
+*/
 ---------------------------------------
 
 -- UPDATE
@@ -59,11 +63,11 @@ WITH ENCRYPTION
 AS
 BEGIN
 	BEGIN TRY
-	IF (@date IS NOT NULL AND @id IS NOT NULL)
-		UPDATE e
-		SET e.date = @date
-		FROM ITI_Exams.dbo.Exam e
-		WHERE e.ID = @id
+		IF (@date IS NOT NULL AND @id IS NOT NULL)
+			UPDATE e
+			SET e.date = @date
+			FROM ITI_Exams.dbo.Exam e
+			WHERE e.ID = @id
 	END TRY  
 	BEGIN CATCH  
 		THROW 500, 'Could not update exam', 16
@@ -71,10 +75,12 @@ BEGIN
 END;
 GO
 -- UPDATE TEST
+/*
 UpdateExam 1, '2022-6-30';
 GO
 UpdateExam NULL, NULL;
 GO
+*/
 ---------------------------------------
 
 
@@ -84,13 +90,13 @@ WITH ENCRYPTION
 AS
 BEGIN
 	BEGIN TRY
-	IF (@date IS NOT NULL OR @id IS NOT NULL)
-		DELETE e
-		FROM ITI_Exams.dbo.Exam e
-		WHERE
-		(@id IS NULL OR e.ID = @id)
-		AND
-		(@date IS NULL OR e.date = @date)
+		IF (@date IS NOT NULL OR @id IS NOT NULL)
+			DELETE e
+			FROM ITI_Exams.dbo.Exam e
+			WHERE
+			(@id IS NULL OR e.ID = @id)
+			AND
+			(@date IS NULL OR e.date = @date)
 	END TRY  
 	BEGIN CATCH  
 		THROW 500, 'Could not delete exam', 16
@@ -98,8 +104,10 @@ BEGIN
 END;
 GO
 -- UPDATE TEST
+/*
 DeleteExam 1, NULL;
 GO
 DeleteExam NULL, '2022-6-30';
 GO
+*/
 ---------------------------------------
