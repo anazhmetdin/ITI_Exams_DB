b6cfbaccf7ce694c6589aaa1a59115efa28307e5
mohaned_saudi <109045550+MohanedSaudi@users.noreply.github.com>
2023-01-15 08:49:56 +0200

    Update Topic_Table.sql

--- SPs/Topic_Table.sql
+++ SPs/Topic_Table.sql
@@ -2,8 +2,7 @@ USE ITI_Exams
 GO
 
 -- SELECT Topic
-CREATE PROC SelectTopic (@id INT = NULL, @tName varchar(30) = NULL)
-WITH ENCRYPTION
+CREATE or Alter  PROC SelectTopic (@id INT = NULL, @tName varchar(30) = NULL)
 AS
 BEGIN
 	BEGIN TRY
@@ -20,20 +19,9 @@ BEGIN
 END
 GO
 
--- SELECT Topic Test
-SelectTopic 
-GO
-SelectTopic 5
-GO
-SelectTopic NULL, 'RDBMS';
-GO
-SelectTopic NULL, '11';
-GO
-
 ---------------------------------------
 -- INSERT Topic
 CREATE PROC InsertTopic (@tName varchar(16), @id INT OUTPUT)
-WITH ENCRYPTION
 AS
 BEGIN
 	BEGIN TRY
@@ -46,19 +34,9 @@ BEGIN
 END;
 GO
 
--- INSERT Topic Test
-INSERT INTO ITI_Exams.dbo.Topic (Name) VALUES ('HTML')
-GO
-DECLARE @topic_id INT;
-exec InsertTopic 'HTML', @topic_id OUTPUT;
-GO
-InsertTopic NULL;
-GO
-
 ---------------------------------------
 -- UPDATE Topic with 2 options by id or by name
-CREATE PROC UpdateTopic (@id INT, @oldName varchar(30), @newName varchar(30))
-WITH ENCRYPTION
+CREATE or Alter PROC UpdateTopic (@id INT, @oldName varchar(30), @newName varchar(30))
 AS
 BEGIN
 	BEGIN TRY
@@ -68,7 +46,7 @@ BEGIN
 			SET t.Name = @newName
 			FROM ITI_Exams.dbo.Topic as t
 			WHERE t.ID = @id or t.Name = @oldName
-        END
+                END
 		ELSE
 		select 'Could not update topic'
 	END TRY  
@@ -78,24 +56,9 @@ BEGIN
 END;
 GO
 
--- UPDATE TEST
-SelectTopic 6      -- this is RDBMS
-GO
-UpdateTopic NULL, 'RDBMS', 'rdbms';
-GO
-SelectTopic 6      -- Now, this became rdbmms after update
-GO
-UpdateTopic 5, NULL, 'MSS';  -- this is Sql Server
-GO
-SelectTopic 6        -- Now, this became MSS after update
-GO
-UpdateTopic NULL, NULL, 'OSEO'; -- Could not Update topic
-GO
-
 ---------------------------------------
 -- DELETE Topic with 2 options by id or by name
 CREATE OR ALTER PROC DeleteTopic (@id INT , @tName varchar(30))
-WITH ENCRYPTION
 AS
 BEGIN
 	BEGIN TRY
@@ -104,7 +67,7 @@ BEGIN
 			DELETE t
 			FROM ITI_Exams.dbo.Topic as t
 			WHERE t.ID = @id or t.Name = @tName
-        END
+                END
 		ELSE
 		select 'Could not update topic'
 	END TRY  
@@ -114,12 +77,3 @@ BEGIN
 END;
 GO
 
--- DELETE TEST
-SelectTopic 9      -- this is CSS
-GO
-DeleteTopic 9, NULL;   -- Delete CSS 
-GO
-SelectTopic 5      -- Now, this became OSE after update
-GO
-DeleteTopic NULL, 'rdbms';  -- this delete rdbms which has an ID = 6
-GO
